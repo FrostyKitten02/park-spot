a472b816936b3c5ba5cb0b67d333c054ebe992d9
FrostyKitten <alen.fridau02@gmail.com>
2025-06-03 15:54:31 +0200

    car modal actually save

--- app/AddCarModal.tsx
+++ app/AddCarModal.tsx
@@ -2,8 +2,14 @@ import {useState} from 'react';
 import {Alert, Button, KeyboardAvoidingView, Platform, ScrollView, StyleSheet, Text, View,} from 'react-native';
 import StyledTextInput from '@/components/StyledTextInput';
 import {accentColor, primaryColor, textSecondaryColor} from "@/constants/Colors";
+import {CarStorage} from "@/storage/CarStorage";
+import {useSQLiteContext} from "expo-sqlite";
+import {useNavigation} from "expo-router";
 
 export default function AddCarModal() {
+    const db = useSQLiteContext();
+    const navigation = useNavigation();
+
     const [name, setName] = useState('');
     const [registrationPlate, setRegistrationPlate] = useState('');
     const [color, setColor] = useState('');
@@ -15,10 +21,13 @@ export default function AddCarModal() {
         }
 
         console.log('Saving car:', { name, registrationPlate, color });
+        //TODO add some kind of loading indicator and disable the button
+        CarStorage.saveCar(db, {name: name, registrationPlateNumber: registrationPlate, color: color});
         Alert.alert('Success', 'Car added!');
         setName('');
         setRegistrationPlate('');
         setColor('');
+        navigation.goBack();
     };
 
     return (
